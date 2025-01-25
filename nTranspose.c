6bac9db1a4746f61b127ad1d8758fddb46c75e5e
nikhil-hehe <babitajha322@gmail.com>
2025-01-25 11:41:31 +0530

    Update nTranspose.c

--- nTranspose.c
+++ nTranspose.c
@@ -2,10 +2,10 @@
 #define MAX 100
 
 int main(){
-	int rows,columns,i,j,a[MAX][MAX],b[MAX][MAX],sum=0,temp;
+	int rows,columns,i,j,a[MAX][MAX],b[MAX][MAX],sum=0;
 	printf("enter number of rows and columns\n");
 	scanf("%d%d",&rows,&columns);
-		printf("\nRow Major order\n");
+	printf("\nRow Major order\n");
 	for (i=0;i<rows;i++){
 		for (j=0;j<columns;j++){
 			scanf("%d",&a[i][j]);
